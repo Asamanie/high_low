3fe68196d96a9865dcb90c96f763bf6f204e7f24
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-12 13:18:02 -0500

    add comments to help understand code

--- High_Low.php
+++ High_Low.php
@@ -11,19 +11,20 @@ fwrite(STDOUT, 'Hello Codeup, guess a number between 1-100 ');
 // get the input (number) from user
 $guess = fgets(STDIN);
 
+// check to see if guess is correct 
 while ($guess != $number) {
-//	echo 'Good guess\n';
-//}
 
+	// if the guess is higher than $number then tell the user "lower"
 	if ($guess > $number) {
 	echo "Lower\n";
 	$guess = fgets(STDIN);
 
+	// if the guess is lower than the $number then tell the user "higher"
 	} elseif ($guess < $number) {	
 	echo "Higher\n";
 	$guess = fgets(STDIN);
  }
 }
 
-echo "hooray!\n" ;
-
+//if the guess and the $number are the same say "You have..." and end the game
+echo "You have guessed correclty, play again!\n" ;

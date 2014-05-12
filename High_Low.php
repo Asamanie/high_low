82d57be003566a6b019df59d867ac85c44267e6f
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-12 10:33:25 -0500

    created variables for the first steps and gave instruction

---
+++ High_Low.php
@@ -0,0 +1,10 @@
+<? php
+
+$min = 1;
+$max = 100;
+
+// computer must generate a random folder 
+rand ($min , $max );
+
+// ask user to guess number between 1-100
+fwrite(STDOUT, 'Guess number between 1-100 ');

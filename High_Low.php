e1b4a7dfdd915a60ceafb0d4b725ef434e26159b
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-12 11:37:24 -0500

    Added if and elseif statements

--- High_Low.php
+++ High_Low.php
@@ -1,17 +1,22 @@
 <? php
 
-$min = 1;
-$max = 100;
-
 // computer must generate a random folder 
-rand ($min , $max );
+rand (1 ,100);
+
+$number = rand (1, 100);
 
 // ask user to guess number between 1-100
-fwrite(STDOUT, 'Guess number between 1-100 ');
+fwrite(STDOUT, 'Hello Codeup, guess a number between 1-100 ');
 
 // get the input (number) from user
-$number = fgets(STDIN);
+$guess = fgets(STDIN);
+if $guess == $number;
+	echo 'Good guess';
+{
+elseif ($guess >= $number);
+	echo 'Lower\n';
+elseif ($guess <= $number);
+	echo 'Higher\n';
+}
 
-if $number == $number 
-// output the user's guess (number)
-fwrite(STDOUT, "Hello $first_name\n");
+

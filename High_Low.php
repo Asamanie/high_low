9fb056c43d0662049ddd665c903aa4e256f87e7a
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-12 10:44:58 -0500

    Added guss number for user

--- High_Low.php
+++ High_Low.php
@@ -8,3 +8,10 @@ rand ($min , $max );
 
 // ask user to guess number between 1-100
 fwrite(STDOUT, 'Guess number between 1-100 ');
+
+// get the input (number) from user
+$number = fgets(STDIN);
+
+if $number == $number 
+// output the user's guess (number)
+fwrite(STDOUT, "Hello $first_name\n");

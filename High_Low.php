a02e50c34fef611011157bf3fc9fc0c8ff364445
Andrew Samaniego <andrewsamaniego@A-S.local>
2014-05-12 13:32:31 -0500

    Added guess counter to the game

--- High_Low.php
+++ High_Low.php
@@ -10,9 +10,11 @@ fwrite(STDOUT, 'Hello Codeup, guess a number between 1-100 ');
 
 // get the input (number) from user
 $guess = fgets(STDIN);
+$times_guessed = 0;
 
 // check to see if guess is correct 
 while ($guess != $number) {
+	$times_guessed++;
 
 	// if the guess is higher than $number then tell the user "lower"
 	if ($guess > $number) {
@@ -20,11 +22,12 @@ while ($guess != $number) {
 	$guess = fgets(STDIN);
 
 	// if the guess is lower than the $number then tell the user "higher"
-	} elseif ($guess < $number) {	
+	} 
+	elseif ($guess < $number) {	
 	echo "Higher\n";
 	$guess = fgets(STDIN);
  }
 }
 
 //if the guess and the $number are the same say "You have..." and end the game
-echo "You have guessed correclty, play again!\n" ;
+echo "You have guessed correclty in $times_guessed times, play again!\n" ;
